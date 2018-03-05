bfbeaf78d538c0109979b909e36f635f1bbffa74
Paul P <paulp@hotmail.sk>
2018-03-05 17:31:00 +0100

    implcit conversion and hiding constructor

--- src/main/scala/com/pidanic/tchingt/Five.scala
+++ src/main/scala/com/pidanic/tchingt/Five.scala
@@ -1,24 +1,24 @@
 package com.pidanic.tchingt
 
-final case class Five(value: Int = 5) extends AnyVal {
+final class Five private (val value: Int = 5) extends AnyVal {
   def + (otherVal: Double): Double = value + otherVal
   def + (otherVal: Long): Long = value + otherVal
   def + (otherVal: Float): Float = value + otherVal
 
   // TODO is it ok?
-  def + (otherVal: Char): String = value.toString + otherVal
+  //def + (otherVal: Char): String = value.toString + otherVal
   def + (otherVal: String): String = value.toString + otherVal
+
   def + (otherVal: Five): Int = value + otherVal.value
 
   def high: String = "o/"
 }
 
-//object Five {
-//  private val five = Five()
-//  def apply() = five
-//trait Five {
-//  private val value = 5
-//
-//  override def toString: String = value.toString
-//}
+object Five {
+  implicit def toFive(value: Int) = new Five(value)
+
+  private val five = Five()
+
+  def apply(): Five = five
+}
 
--- src/main/scala/com/pidanic/tchingt/Main.scala
+++ src/main/scala/com/pidanic/tchingt/Main.scala
@@ -1,12 +1,18 @@
 package com.pidanic.tchingt
 
+import com.pidanic.tchingt.Five._
+
 object Main extends App {
   println(Five() + 5)
   println(Five() + Five())
   println(Five() + "5")
   val five = Five()
   val five2 = Five()
+  five + 1
+  five + 1.toByte
   println(five.high)
+  println(1 + five)
+  println(1.high)
 
   println(five == five2)
   //println(five eq five2)
