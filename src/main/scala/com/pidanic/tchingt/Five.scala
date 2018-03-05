485c33edf0e61377bd18c26923150b060a081595
Pavol Pidanič <paulp@hotmail.sk>
2018-03-05 15:38:22 +0100

    start of implementation

--- src/main/scala/com/pidanic/tchingt/Five.scala
+++ src/main/scala/com/pidanic/tchingt/Five.scala
@@ -1,5 +1,24 @@
 package com.pidanic.tchingt
 
-trait Five {
+case class Five(value: Int = 5) extends AnyVal {
+  def + (otherVal: Double): Double = value + otherVal
+  def + (otherVal: Long): Long = value + otherVal
+  def + (otherVal: Float): Float = value + otherVal
 
+  // TODO is it ok?
+  def + (otherVal: Char): String = value.toString + otherVal
+  def + (otherVal: String): String = value.toString + otherVal
+  def + (otherVal: Five): Int = value + otherVal.value
+
+  def high: String = "o/"
 }
+
+//object Five {
+//  private val five = Five()
+//  def apply() = five
+//trait Five {
+//  private val value = 5
+//
+//  override def toString: String = value.toString
+//}
+
